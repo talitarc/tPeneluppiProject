d55312139314e88692aacc1eb12a22f3ea67c955
Talita Peneluppi <tablitarc@gmail.com>
2019-01-12 00:13:40 -0500

    html +css + listen click

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+//import { on } from "cluster";
+
+
+$(document).ready(function() {
+//listen user clicking a button
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        console.log('clicked');
+    })
+//change the clicked button's color
+    const userAnswer = $('')
+//if the answer is right, +1 to the score
+
+//show the answer
+
+//when the quizz is finished, show the result
+
+//hide the input/email and input/submit after btn-email clicked
+
+//show a thank you message after hide the items
+
+
+})
